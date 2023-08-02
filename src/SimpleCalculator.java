586425253031e01e2901f4f50d084491552d513d
yusifyusifov21 <yusif.yusifov50@gmail.com>
2023-08-02 04:02:26 -1200

    SumCaculator

---
+++ src/SimpleCalculator.java
@@ -0,0 +1,36 @@
+public class SimpleCalculator {
+    private double firstNumber;
+    private double secondNumber;
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+
+    }
+    public void setFirstNumber(double number){
+        this.firstNumber=number;
+
+    }
+    public  void setSecondNumber(double number){
+        this.secondNumber=number;
+    }
+    public double getAdditionResult(){
+        return secondNumber+firstNumber;
+    }
+    public double getSubtractionResult(){
+        return firstNumber-secondNumber;
+    }
+    public  double getMultiplicationResult (){
+        return firstNumber*secondNumber;
+    }
+    public double getDivisionResult(){
+        if (secondNumber !=0){
+            return firstNumber/secondNumber;
+
+        }else {
+            return 0;
+        }
+    }}
